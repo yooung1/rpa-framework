b5934f87d8241641790bf18620bbb8ec58f1c958
Samuel Pierre <87836619+yooung1@users.noreply.github.com>
2024-11-28 10:14:09 -0300

    Update create_general_variables.py
    dev: TODO de coisas a fazer na logica

--- framework/initialize_environment/modules/create_general_variables.py
+++ framework/initialize_environment/modules/create_general_variables.py
@@ -1,4 +1,23 @@
-def create_general_variables():
-    # TODO: Pegar todos os dados vindos do config.json e criar uma variavel com os valores desejados
-    # VER MAIS INFORMAÇÕES NO README.md
-    pass
+class CreateGeneralVariables:
+    def __init__(self):
+        # TODO: colocar os dicionarios de init
+        # TODO: eu irei instanciar esta classe com as variaveis inicializadas e sempre que um queue item for finalizado eu irei chamar a instancia e 
+        # adicionar os dados de logs gerados por aquele queue_item
+        self.errors_data_table = {}
+        self.success_or_bre_data_table = {}
+    def create_general_variables():
+        # TODO: Pegar todos os dados vindos do config.json e criar uma variavel com os valores desejados
+        # VER MAIS INFORMAÇÕES NO README.md
+        # As informações que esta função ira conter sera em formato Dict como no exemplo abaixo, sera possivel enviar os logs do processo e no final iremos transformar
+        # isso numa planilha excel contendo os dados - e essa planilha sera enviada para o usuario final
+        # AQUI SERA A PARTE DO CODIGO EM QUE O DEV deve adicionar a sua propria logica segundo o seu processo
+        pass
+        # dict_with_info = {
+        #     "email":"",
+        #     "password":"",
+        #     "username":"",
+        # }
+    
+        # dict_with_errors = {
+        #     "error_list":{}
+        # }
